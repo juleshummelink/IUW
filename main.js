891f2590be41f9ca182bea6a188c7df2ba18d887
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-12-02 15:40:27 +0100

    a test

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ function redirect(url){
     openers[1].style.animation = "closeRight 0.5s forwards";
     setTimeout(function(){
         console.log(url);
-        //window.location.href = url;
+        window.location.replace(url);
     }, 500);
 }
