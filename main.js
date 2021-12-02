f9cd3d6824c4557a71d1513fa9aa4ac8cfeaee1c
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-12-02 19:27:04 +0100

    Added a responsive mobile menu!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const openers = document.getElementsByClassName("open");
 
 const mobileButton = document.getElementsByClassName("mobileButton")[0];
+const mobileMenuObject = document.getElementsByClassName("mobileMenu")[0];
 
 var menuIsOpen = false;
 
@@ -15,9 +16,11 @@ function redirect(url){
 function mobileMenu(){
     if(!menuIsOpen){
         mobileButton.src = "img/close.svg";
+        mobileMenuObject.style.maxHeight = "200px";
         menuIsOpen = true;
     } else {
         mobileButton.src = "img/menu.svg";
+        mobileMenuObject.style.maxHeight = "0px";
         menuIsOpen = false;
     }
 }
