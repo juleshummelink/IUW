16bc15c7d227df20a3bd7c336294482e8b7d8f4d
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-12-02 16:35:19 +0100

    Mobile button

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const openers = document.getElementsByClassName("open");
 
+const mobileButton = document.getElementsByClassName("mobileButton")[0];
+
+var menuIsOpen = false;
+
 function redirect(url){
     openers[0].style.animation = "closeLeft 0.5s forwards";
     openers[1].style.animation = "closeRight 0.5s forwards";
     setTimeout(function(){
         window.location.replace(url);
     }, 500);
+}
+
+function mobileMenu(){
+    if(!menuIsOpen){
+        mobileButton.src = "img/close.svg";
+        menuIsOpen = true;
+    } else {
+        mobileButton.src = "img/menu.svg";
+        menuIsOpen = false;
+    }
 }
