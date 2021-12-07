e79b2a03f1dfe2c1ab120a9c9d50118ceae30d7f
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-12-07 12:28:49 +0100

    Worked on contact backend and frontend

---
+++ send.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+each("Dear " . $name . ". We have received your message! We will contact you soon at " . $email);
+
+?>
