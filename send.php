db5720654e52c855f77d96de287a54b36bcd0279
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-12-07 15:51:51 +0100

    Added file writing to the php code!

--- send.php
+++ send.php
@@ -1,9 +1,19 @@
 <?php
-
+// Get form data
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
+// Create the file 
+$filename = $name . ".txt";
+$file = fopen($filename, "W");
+$fileContent = "Please contact:\n" . $name . " on " . $email . "\nMessage:\n" . $message;
+fwrite($file, $fileContent);
+fclose($file);
+
+// Send email
+
+// Return a message for the user
 echo "Dear " . $name . ". We have received your message! We will contact you soon at " . $email;
 
 ?>
