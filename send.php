fd09399c494032b489e2140ba240cc0eefcaa215
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-12-07 12:45:53 +0100

    php

--- send.php
+++ send.php
@@ -4,6 +4,6 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-each "Dear " . $name . ". We have received your message! We will contact you soon at " . $email;
+echo "Dear " . $name . ". We have received your message! We will contact you soon at " . $email;
 
 ?>
